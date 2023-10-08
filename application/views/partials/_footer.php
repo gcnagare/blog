76b819a1dca767bd6023109621498d57ea348341
gcnagare <147309909+gcnagare@users.noreply.github.com>
2023-10-08 15:19:21 +0530

    Add files via upload

---
+++ application/views/partials/_footer.php
@@ -0,0 +1,12 @@
+    <footer class="footer">
+        <section class="hero is-info is-medium is-bold">
+        <div>
+            <div class="container has-text-centered">
+                <div class="subtitle" style="font-weight: 800">UNIFIED VOICE COMMUNICATION Machine Test By GANESH NAGARE</div>
+            </div>
+        </div>
+    </section>
+    </footer>
+    <script async type="text/javascript" src="<?=base_url('public/js/burger.js')?>"></script>
+</body>
+</html>
